03066f4b0cbbadb9752a726c754858d1ef8d000f
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-01-16 15:23:37 -0300

    Criação de classe para Série. Criação de Enum para Categoria.

---
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -0,0 +1,64 @@
+package br.com.alura.screenmatch.model;
+
+import java.util.OptionalDouble;
+
+public class Serie {
+	private String titulo;
+	private Integer totalTemporadas;
+	private Double avaliacao;
+	private Categoria genero;
+	private String atores;
+	private String poster;
+	private String sinopse;
+
+	public Serie(DadosSerie dadosSerie) {
+		this.titulo = dadosSerie.titulo();
+		this.totalTemporadas = dadosSerie.totalTemporadas();
+		this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0.0);
+		this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+		this.atores= dadosSerie.atores();
+		this.poster = dadosSerie.poster();
+		this.sinopse= dadosSerie.sinopse();
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public Integer getTotalTemporadas() {
+		return totalTemporadas;
+	}
+
+	public Double getAvaliacao() {
+		return avaliacao;
+	}
+
+	public Categoria getGenero() {
+		return genero;
+	}
+
+	public String getAtores() {
+		return atores;
+	}
+
+	public String getPoster() {
+		return poster;
+	}
+
+	public String getSinopse() {
+		return sinopse;
+	}
+
+	@Override
+	public String toString() {
+		return "titulo=" + titulo +
+				", totalTemporadas=" + totalTemporadas +
+				", avaliacao=" + avaliacao +
+				", genero=" + genero +
+				", atores=" + atores +
+				", poster=" + poster +
+				", sinopse=" + sinopse;
+	}
+	
+	
+}
